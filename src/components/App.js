50adc11d7983b479ee85b04a47b72ee8d77ae48a
Francisco González <francisco.gonzalez.lorca@gmail.com>
2021-04-21 17:49:55 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 
-const KEY = process.env.KEY;
+const KEY = process.env.REACT_APP_KEY;
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
